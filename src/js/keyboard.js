d1c84af2404998869b37abf5aef793df3d8848d8
최철웅 <105634994+hi-woong@users.noreply.github.com>
2022-10-26 19:55:04 +0900

    keyboard event

--- src/js/keyboard.js
+++ src/js/keyboard.js
@@ -1,27 +1,65 @@
 export class Keyboard {
   #swichEl;
   #fontSelectEl;
+  #containerEl;
+  #keyboardEl;
+  #inputGroupEl;
+  #inputEl;
+
   constructor() {
     this.#assignElement();
     this.#addEvent();
   }
 
   #assignElement() {
-    this.#swichEl = document.getElementById("switch");
-    this.#fontSelectEl = document.getElementById("font");
+    this.#containerEl = document.getElementById("container");
+    this.#swichEl = this.#containerEl.querySelector("#switch");
+    this.#fontSelectEl = this.#containerEl.querySelector("#font");
+    this.#keyboardEl = this.#containerEl.querySelector("#keyboard");
+    this.#inputGroupEl = this.#containerEl.querySelector("#input-group");
+    this.#inputEl = this.#inputGroupEl.querySelector("#input");
   }
 
   #addEvent() {
-    this.#swichEl.addEventListener("change", (event) => {
-      document.documentElement.setAttribute(
-        "theme",
-        event.target.checked ? "dark-mode" : ""
-      );
-      console.log(event.target.checked);
-    });
-    this.#fontSelectEl.addEventListener("change", (event) =>{
-        document.body.style.fontFamily = event.target.value;
-        console.log(event.target.value);
-    })
+    this.#swichEl.addEventListener("change", this.#onChangeTheme);
+    this.#fontSelectEl.addEventListener("change", this.#onChangeFont);
+
+    document.addEventListener("keydown", this.#onKeyDown.bind(this));
+    document.addEventListener("keyup", this.#onKeyUp.bind(this));
+
+    this.#inputEl.addEventListener("input", this.#onInput);
+  }
+
+  #onInput(event) {
+    event.target.value = event.target.value.replace(/[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/, "");
+  }
+
+  #onKeyDown(event) {
+    this.#keyboardEl
+    .querySelector(`[data-code=${event.code}]`)
+    ?.classList.add("active");
+  this.#inputGroupEl.classList.toggle(
+    "error",
+    /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/.test(event.key)
+  );
+
+   
+  }
+
+  #onKeyUp(event) {
+    this.#keyboardEl
+      .querySelector(`[data-code=${event.code}]`)
+      ?.classList.remove("active"); //optional chaning 으로 대체가능 언디파인드 리턴
+  }
+
+  #onChangeTheme(event) {
+    document.documentElement.setAttribute(
+      "theme",
+      event.target.checked ? "dark-mode" : ""
+    );
+  }
+
+  #onChangeFont(event) {
+    document.body.style.fontFamily = event.target.value;
   }
 }
